879d5aaa39759031a50056ea2bfcb48df2676545
yannisarc <yannis.kyrillos@athenarc.gr>
2025-11-25 17:33:37 +0200

    test: fix userService tests

--- src/test/groovy/com/studio/movierama/UserServiceSpec.groovy
+++ src/test/groovy/com/studio/movierama/UserServiceSpec.groovy
@@ -1,17 +1,19 @@
 package com.studio.movierama
 
+import com.studio.movierama.converter.UserDtoToUserConverter
+import com.studio.movierama.converter.UserToUserDtoConverter
 import com.studio.movierama.domain.User
 import com.studio.movierama.dto.UserDto
 import com.studio.movierama.repository.UserRepository
 import com.studio.movierama.service.UserService
-import org.springframework.core.convert.ConversionService
 import spock.lang.Specification
 
 class UserServiceSpec extends Specification {
 
     UserRepository userRepository = Mock(UserRepository)
-    ConversionService conversionService = Mock(ConversionService)
-    UserService userService = new UserService(userRepository, conversionService)
+    UserToUserDtoConverter userToUserDtoConverter = Mock(UserToUserDtoConverter)
+    UserDtoToUserConverter userDtoToUserConverter = Mock(UserDtoToUserConverter)
+    UserService userService = new UserService(userRepository, userDtoToUserConverter, userToUserDtoConverter)
 
     def "save"() {
         given: "a request to save a user"
@@ -20,8 +22,8 @@ class UserServiceSpec extends Specification {
             userService.save(userDto)
         then: "the repository method is called once & conversionService twice"
             1 * userRepository.save(_)
-            1 * conversionService.convert(_, User.class)
-            1 * conversionService.convert(_, UserDto.class)
+            1 * userDtoToUserConverter.convert(_)
+            1 * userToUserDtoConverter.convert(_)
     }
 
     def "find by username"() {
@@ -29,6 +31,6 @@ class UserServiceSpec extends Specification {
             userService.findByUsername("username")
         then: "repository and conversionService are each called once"
             1 * userRepository.findByUsername("username") >> Optional.of(new User())
-            1 * conversionService.convert(_, UserDto.class)
+            1 * userToUserDtoConverter.convert(_)
     }
 }
